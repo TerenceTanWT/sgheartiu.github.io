c34cd4beb5231db6c399b97bf17ca1a3708f69cd
Terence <Terence@Terences-MacBook-Pro.local>
2019-01-07 00:03:07 +0800

    Attempt to fix web app open URL in new tab

--- _site/assets/js/pwa-url.js
+++ _site/assets/js/pwa-url.js
@@ -1,17 +1,3 @@
 // Prevent links from opening in Safari when in web app mode (PWA)
 
-//(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
-
-var eventHandler = function (event) {
-    // Only run for iOS full screen apps
-    if (('standalone' in window.navigator) && window.navigator.standalone) {
-        // Only run if link is an anchor and points to the current page
-        if ( event.target.tagName.toLowerCase() !== 'a' || event.target.hostname !== window.location.hostname || event.target.pathname !== window.location.pathname || !/#/.test(event.target.href) ) return;
-
-        // Open link in same tab
-        event.preventDefault();
-        window.location = event.target.href;
-        alert("Hello");
-    }
-}
-window.addEventListener('click', eventHandler, false);
+(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
