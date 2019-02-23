6a49adf649b9827a1a141f1ca534c2b16975489d
Terence <Terence@Terences-MacBook-Pro.local>
2019-02-24 00:36:00 +0800

    Implemented popup banner
    Implemented popup banner for future use

---
+++ assets/js/overlay-terence.js
@@ -0,0 +1,13 @@
+// Added by @TerenceTanWT for banner overlay feature
+
+function overlayOn() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function overlayOff() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function overlayRedirect() {
+	window.open("https://www.facebook.com/sgheartiu", "_blank");
+}
