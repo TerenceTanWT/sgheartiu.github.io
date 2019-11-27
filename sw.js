3cee525e16181caed767f10c24bf06f82f93493a
TerenceTanWT <e0357478@u.nus.edu>
2019-11-28 05:46:29 +0800

    Update manifest

--- sw.js
+++ sw.js
@@ -10,8 +10,27 @@ self.addEventListener('install', function(e) {
        '/about/',
        '/projects/',
        '/concert-2018/',
-       '/22nd-birthday-2014/',
-       '/assets/'
+       '/blue/',
+       '/assets/css/main.scss',
+       '/assets/css/overlay-terence.scss',
+       '/assets/files/fanchant-concert-2018.pdf',
+       '/assets/js/cache-polyfill.js',
+       '/assets/js/jquery-1.12.0.min.js',
+       '/assets/js/jquery.backstretch.min.js',
+       '/assets/js/jquery.dlmenu.min.js',
+       '/assets/js/jquery.fitvid.min.js',
+       '/assets/js/jquery.goup.min.js',
+       '/assets/js/jquery.magnific-popup.min.js',
+       '/assets/js/modernizr-3.3.1.custom.min.js',
+       '/assets/js/NoSleep.min.js',
+       '/assets/js/overlay-terence.js',
+       '/assets/js/pwa-url.js',
+       '/assets/fonts/FiraSans/woff/FiraSans-Light.woff',
+       '/assets/fonts/FiraSans/woff/FiraSans-LightItalic.woff',
+       '/assets/fonts/FiraSans/woff/FiraSans-Regular.woff',
+       '/assets/fonts/FiraSans/woff/FiraSans-Bold.woffs',
+       '/assets/img/placeholder-big.jpg',
+       '/assets/img/logo.pngs'
      ]);
    })
  );
